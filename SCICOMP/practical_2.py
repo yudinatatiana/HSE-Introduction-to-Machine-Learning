696c6cb0b374b11ca0b490279b9cc80164bb62f7
Tatiana <33958152+yudinatatiana@users.noreply.github.com>
2021-06-17 14:42:30 +0300

    Add files via upload

---
+++ SCICOMP/practical_2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Exercise: use python to check this:
+
+# In[1]:
+
+
+from decimal import Decimal
+
+print(repr(Decimal(0.1)))
+
+
+# Exercise: what is the best approximation of 0.01?
+
+# In[2]:
+
+
+print(repr(Decimal(0.01)))
+
+
+# Exercise: implement the Caesar cipher in python, which advances each letter of 'M' by 'SEC = n': enc(1, "a") = "b", etc.
+
+# In[8]:
+
+
+def caesar_cipher(text, step, alphabets):
+
+    def shift(alphabet):
+        return alphabet[step:] + alphabet[:step]
+
+    shifted_alphabets = tuple(map(shift, alphabets))
+    joined_aphabets = ''.join(alphabets)
+    joined_shifted_alphabets = ''.join(shifted_alphabets)
+    table = str.maketrans(joined_aphabets, joined_shifted_alphabets)
+    return text.translate(table)
+
+
+# In[11]:
+
+
+import string
+alphabets = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+caesar_cipher('tanya22', step=4, alphabets=alphabets)
+
+
+# In[ ]:
+
+
+
+
